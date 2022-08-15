6f04a6ce9c681bccce01e85f62c35216598c0974
khashayar7575 <ashurikhashayar@gmail.com>
2022-08-16 01:00:52 +0430

    fix the problem of showing index page

--- pages/car_body_insurance.tsx
+++ pages/car_body_insurance.tsx
@@ -3,16 +3,40 @@ import { BreadCrumb } from "../exteras/components/BreadCrumb";
 import { CarBodyInsuranceComp } from "../exteras/components/carbodyinsurance/CarBodyInsuranceComp";
 import { Footer } from "../exteras/components/desktop/Footer";
 import { Navbar } from "../exteras/components/Navbar";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import {useTranslation} from "next-i18next"
+import {GetStaticProps} from "next";
+
+
+export  const getStaticProps : GetStaticProps = async ({ locale} : any) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
+
 
 const CarBodyInsurance = () => {
   const [loginVisible, setLoginVisible] = useState<boolean>(false);
+  const {t} = useTranslation("common")
 
   const handleLogin = () => {
     setLoginVisible(!loginVisible);
   };
   return (
     <>
-      <Navbar handleLogin={handleLogin} />
+      <Navbar 
+      LiabilityInsurance={t("LiabilityInsurance")}  
+      PropertyInsurance={t("Property insurance")} 
+      VehicleInsurance={t("VehicleInsurance")} 
+      DamageOnline={t("DamageOnline")}
+      PeopleInsurance={t("PeopleInsurance")}
+      InsuranceCompanies={t("InsuranceCompanies")}
+      Blog={t("Blog")}
+      SignUp={t("SignUp/Login")}
+      handleLogin={handleLogin} 
+      />
       <BreadCrumb title={"car_body_insurance"} />
       <div className="padding-2">
         <CarBodyInsuranceComp />
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,21 +10,63 @@ import { Footer } from "../exteras/components/desktop/Footer";
 import React from "react";
 import { useRouter } from "next/router";
 import {GetStaticProps} from "next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import {useTranslation} from "next-i18next"
+
+export  const getStaticProps : GetStaticProps = async ({ locale} : any) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
 
 
-
-const Home: NextPage = () => (
+const Home: NextPage = () => {
+  const {t} = useTranslation("common"); 
+  return(
   <>
     {/* <Navbar /> */}
-    <Hero />
-    <InsuranceCompanies />
-    <OnlineServices />
+    <Hero
+     Insurance={t("Insurance")}
+     CompareAndBuyInsuranceOnline={t("CompareAndBuyInsuranceOnline")} 
+     companySlogan={t("ThePossibilityOfEasyComparisonAndSuitableChoiceAmongDifferentInsuranceCompaniesAccordingToYourConditions")}
+     CompareInsuranceCompanies={t("CompareInsuranceCompanies")}
+    />
+    <InsuranceCompanies
+     carBody={t("carBody")} 
+     ThirdParty={t("ThirdParty")}
+     Motorcycle={t("Motorcycle")}
+     Earthquake={t("Earthquake")}
+     Fire={t("Fire")} 
+     Travel={t("Travel")}
+     groupHealth={t("groupHealth")}
+     life={t("life")}
+     health={t("health")}
+     medical={t("medical")}
+     view={t("viewAllInsurances")}
+     safe={t("SafeAndAffordablePurchase")}
+     getInsurance={t("GetTheInsuranceYouWantEasilyAndAtAReasonablePrice")}
+     HourSupport={t("24HourSupport")}
+     haveQues={t("IfYouHaveAnyQuestionsOrDoubts,YouCanContactOurSupport.")}
+     choose={t("ChoosingTheMostSuitableInsurance")}
+     possibility={t("ThereAreManyPossibilitiesToCompareAndPrepareYourDesiredInsurance")}
+    />
+    <OnlineServices 
+     OnlineServices={t("OnlineServices")}
+     OnlineTreatment ={t("OnlineTreatmentCompensation")}
+     PaymentOfTreatment={t("PaymentOfTreatmentFromInsurance")}
+     RequestRegistration={t("RequestRegistration")}
+     OnlineCarDamagePayment={t("OnlineCarDamagePayment")}
+     PaymentOfDamages={t("PaymentOfDamagesInThreeStages")}
+    />
     <Blog />
     <Comments />
     <Companies />
     <Footer />
   </>
-);
+  )
+};
 
 export default Home;
 
